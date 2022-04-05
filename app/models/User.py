a918b66cae91d026661ceced33367255f6140181
Supergrammer <supergrammer@nhn.com>
2022-04-05 22:52:02 +0900

    [Feat] DB User 스키마 작성, #2

--- app/models/User.py
+++ app/models/User.py
@@ -1,6 +1,11 @@
-import email
-from pydantic import BaseModel
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 
-class User(BaseModel):
-    email: str
-    password: str
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    email = Column(String, primary_key=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
