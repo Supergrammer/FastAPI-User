cdae73b36d0d6bbf3ebc15055dd0e9ec79a21751
Supergrammer <supergrammer@nhn.com>
2022-04-08 21:45:59 +0900

    [Feat] main.py 에 user_model 생성 코드 추가, #2

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
-from typing import Optional
-
 from app.routers import user_account
+from app.models import user_model
+from app.configurations.database import engine
 
 from fastapi import FastAPI
 
+user_model.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(user_account.router)
-
 
 @app.get("/")
 def read_root():
