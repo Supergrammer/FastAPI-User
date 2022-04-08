021c75ff81770105ca18ad09a2a68efefe7c0058
Supergrammer <supergrammer@nhn.com>
2022-04-08 21:41:45 +0900

    [Feat] User model 변경 및 DB 연결, #2

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,11 +1,13 @@
+from unicodedata import name
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 
-from app.database import Base
-
+from app.configurations.database import Base
 
 class User(Base):
     __tablename__ = "user"
 
     email = Column(String, primary_key=True, index=True)
     hashed_password = Column(String)
+    username = Column(String)
+    nickname = Column(String)
     is_active = Column(Boolean, default=True)
