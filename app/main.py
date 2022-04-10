a40eb95ef1394fc77b415d446c67e2c5cd5ef2d7
Supergrammer <supergrammer@nhn.com>
2022-04-10 19:24:43 +0900

    [Chore] main.py 파일 정리, #3

--- app/main.py
+++ app/main.py
@@ -1,19 +1,10 @@
-from app.routers import user_account
-from app.models import user_model
-from app.configurations.database import engine
-
 from fastapi import FastAPI
 
-user_model.Base.metadata.create_all(bind=engine)
+from app.routers import routers
+from app.models import models
+from app.configurations.database import engine
+
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
-
-app.include_router(user_account.router)
-
-@app.get("/")
-def read_root():
-    return {"item": 123123123}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
+app.include_router(router=routers.router)
