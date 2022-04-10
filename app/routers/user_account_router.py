7fce91aab79393b96877e91c3edef664b45ed3af
Supergrammer <supergrammer@nhn.com>
2022-04-10 19:23:23 +0900

    [Feat] User CRUD 추가 및 router 명 변경, #4

---
+++ app/routers/user_account_router.py
@@ -0,0 +1,55 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.configurations.database import get_db
+from app.services import user_account_service
+
+from app.schemas import user_schema
+
+router = APIRouter(
+    prefix="/users",
+    tags=["user"],
+)
+
+db = Depends(get_db)
+
+
+@router.post("", response_model=user_schema.User)
+async def create_user(user: user_schema.UserCreate, db: Session = db):
+    db_user = user_account_service.get_user(db=db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="E-mail already exists.")
+
+    return user_account_service.create_user(db=db, user=user)
+
+
+@router.get("", response_model=user_schema.User)
+async def read_user(email: str, db: Session = db):
+    db_user = user_account_service.get_user(db=db, email=email)
+    if not db_user:
+        raise HTTPException(status_code=400, detail="E-mail does not exist.")
+    
+    return db_user
+
+
+@router.put("", response_model=user_schema.User)
+async def update_user(user: user_schema.UserCreate, db: Session = db):
+    db_user = user_account_service.get_user(db=db, email=user.email)
+    if not db_user:
+        raise HTTPException(status_code=400, detail="E-mail does not exist.")
+
+    return user_account_service.update_user(db=db, user=user)
+
+
+@router.delete("", response_model=user_schema.User)
+async def delete_user(email: str, db: Session = db):
+    db_user = user_account_service.get_user(db=db, email=email)
+    if not db_user:
+        raise HTTPException(status_code=400, detail="E-mail does not exist.")
+
+    return user_account_service.delete_user(db=db, email=email)
+
+
+@router.get("/all", response_model=list[user_schema.User])
+async def read_all_users(skip: int | None = None, limit: int | None = None, db: Session = db):
+    return user_account_service.get_all_users(db=db, skip=skip, limit=limit)
