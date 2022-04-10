327aaeed80c3a15d91db2fd8fc4efaf2bae3c426
Supergrammer <supergrammer@nhn.com>
2022-04-10 19:22:02 +0900

    [Feat] BaseSchema, User, UserCreate pydantic 클래스 추가, #4

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,13 +1,29 @@
 from pydantic import BaseModel
+from datetime import datetime
+
+
+class BaseSchema(BaseModel):
+    created_date: datetime
+    updated_date: datetime
 
 
 class UserBase(BaseModel):
     email: str
+
+
+class User(UserBase, BaseSchema):
     username: str
     nickname: str
-    is_active: str
+    is_active: bool
 
-class User(UserBase):
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(UserBase):
+    password: str
+    username: str
+    nickname: str
 
     class Config:
         orm_mode = True
