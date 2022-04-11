f2a03b5df799d15e57498ed33462b0069a058dfe
Supergrammer <supergrammer@naver.com>
2022-04-11 22:19:39 +0900

    [Feat] user_auth_router 라우터에 추가, #5

--- app/routers/routers.py
+++ app/routers/routers.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from .user_account_router import router as user_account_router
+from .user_auth_router import router as user_auth_router
 
 router = APIRouter()
 
 router.include_router(user_account_router)
+router.include_router(user_auth_router)
