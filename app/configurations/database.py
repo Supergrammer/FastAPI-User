9aa82ca479d5b5d449d8143b68057fc34bf7e3b2
Supergrammer <supergrammer@naver.com>
2022-04-15 19:40:34 +0900

    [Feat] DB settings 를 읽어와 postgres 연결, #3

--- app/configurations/database.py
+++ app/configurations/database.py
@@ -2,12 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-POSTGRES_DATABASE_URL = "postgresql://fastapi_user:rlaguscjf@127.0.0.1/user_db"
+from .settings import get_database_settings
 
-engine = create_engine(POSTGRES_DATABASE_URL)
+db = get_database_settings()
+
+DATABASE_URL = f"{db.database}://{db.database_user}:{db.database_password}@{db.database_server}/{db.database_name}"
+
+engine = create_engine(DATABASE_URL)
 db_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
 
 def get_db():
     db = db_session()
