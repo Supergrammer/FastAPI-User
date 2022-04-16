fb4d1b1730458ae8bd0ccf04ed1ef960aa6e16d5
Supergrammer <supergrammer@naver.com>
2022-04-16 19:03:15 +0900

    [Feat] runserver.sh 실행 스크립트 수정, #3

--- runserver.sh
+++ runserver.sh
@@ -2,5 +2,20 @@
 # conda create --file environment.yaml
 # conda activate FastAPI-User
 
-### Run FastAPI-User Back-end Server
-uvicorn app.main:app --reload
+if [ $# -ne 1 ]; then
+    echo "[Usage] runserver.sh mode (development/production)"
+    exit 0
+fi
+
+if [ $1 = "development" ]; then
+    ### Run FastAPI-User Back-end Server development mode
+    MODE="development" uvicorn app.main:app --reload
+
+elif [ $1 = "production" ]; then
+    ### Run FastAPI-User Back-end Server production mode
+    MODE="production" uvicorn app.main:app --host 0.0.0.0 --port 80 --workers 4
+    
+else
+    echo "[Usage] runserver.sh mode (development/production)"
+    exit 0
+fi
