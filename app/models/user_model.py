0d6d5211aef11b52e0189acb2c5080d4f187cad8
Supergrammer <supergrammer@naver.com>
2022-04-16 19:06:41 +0900

    [Feat] user_model 에 password relationship 추가, #6

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,4 +1,8 @@
 from sqlalchemy import Column, String, Boolean
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+
+import uuid
 
 from .base_model import Base, BaseMixin
 
@@ -6,8 +10,11 @@ from .base_model import Base, BaseMixin
 class User(Base, BaseMixin):
     __tablename__ = "user"
 
-    email = Column(String, primary_key=True, index=True, nullable=False)
-    password = Column(String, nullable=False)
+    id = Column(UUID(as_uuid=True), primary_key=True,
+                nullable=False, index=True, default=uuid.uuid4)
+    email = Column(String, unique=True, nullable=False, index=True)
+    password = relationship("Password", backref="user", uselist=False,
+                            primaryjoin="foreign(User.id) == Password.id")
     username = Column(String, nullable=False)
     nickname = Column(String, nullable=True)
-    is_active = Column(Boolean, default=True, nullable=False)
+    is_active = Column(Boolean, nullable=False, default=True)
