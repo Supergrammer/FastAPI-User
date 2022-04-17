ec3219a399418551900eeb90bbdd28c4514fb5fa
Supergrammer <supergrammer@naver.com>
2022-04-17 16:22:55 +0900

    [Feat] user_account_service 수정, #6

--- app/services/user_account_service.py
+++ app/services/user_account_service.py
@@ -1,5 +1,4 @@
 from sqlalchemy.orm import Session
-import uuid
 
 from app.models.user_model import User
 from app.models.password_model import Password
@@ -9,13 +8,16 @@ from app.schemas import user_schema
 from app.modules import auth_module
 
 
-def create_user(db: Session, user: user_schema.UserCreate):
-    id = uuid.uuid4()
+### Create User
+def create_user(db: Session, user: user_schema.Request.UserCreate):
     hashed_password = auth_module.get_hashed_password(user.password)
-    password = Password(id=id, hashed_password=hashed_password)
 
-    db_user = User(id=id, email=user.email, password=password,
-                   username=user.username, nickname=user.nickname)
+    db_user = User(
+        email=user.email,
+        password=Password(hashed_password=hashed_password),
+        username=user.username,
+        nickname=user.nickname
+    )
 
     db.add(db_user)
     db.commit()
@@ -24,18 +26,20 @@ def create_user(db: Session, user: user_schema.UserCreate):
     return db_user
 
 
-def get_user(db: Session, email: str):
+### Get User By Email
+def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email == email).first()
 
 
+### Get All Users
 def get_all_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(User).offset(skip).limit(limit).all()
 
 
-def update_user(db: Session, user: user_schema.UserCreate):
-    db_user = get_user(db=db, email=user.email)
+### Update User
+def update_user(db: Session, user: user_schema.Request.UserCreate):
+    db_user = get_user_by_email(db=db, email=user.email)
 
-    db_user.password = user.password
     db_user.username = user.username
     db_user.nickname = user.nickname
 
@@ -45,7 +49,7 @@ def update_user(db: Session, user: user_schema.UserCreate):
 
 
 def delete_user(db: Session, email: str):
-    db_user = get_user(db=db, email=email)
+    db_user = get_user_by_email(db=db, email=email)
 
     db.delete(db_user)
     db.commit()
