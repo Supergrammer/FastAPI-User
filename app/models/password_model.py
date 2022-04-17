f289261968397ce2da9fc42d05eddf50961c6c98
Supergrammer <supergrammer@naver.com>
2022-04-17 16:21:26 +0900

    [Feat] Password model default id 값 추가, #6

--- app/models/password_model.py
+++ app/models/password_model.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, String, Boolean
 from sqlalchemy.dialects.postgresql import UUID
 
+import uuid
+
 from .base_model import Base, BaseMixin
 
 
@@ -8,6 +10,6 @@ class Password(Base, BaseMixin):
     __tablename__ = "password"
 
     id = Column(UUID(as_uuid=True), primary_key=True,
-                nullable=False, index=True)
+                nullable=False, index=True, default=uuid.uuid4)
     hashed_password = Column(String, nullable=False)
     is_expired = Column(Boolean, nullable=False, default=False)
