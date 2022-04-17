04037417b002e7172a8294def943bcb708b88024
Supergrammer <supergrammer@naver.com>
2022-04-17 18:36:54 +0900

    [Feat] User Account CRUD 에 액세스 토큰 로직 추가, #5

--- app/routers/user_account_router.py
+++ app/routers/user_account_router.py
@@ -6,6 +6,10 @@ from app.services import user_account_service
 
 from app.schemas import user_schema
 
+from app.modules.auth_module import get_current_user
+
+from app.http_exception import user_already_exist_exception, user_not_exist_exception
+
 
 router = APIRouter(
     prefix="/users",
@@ -15,56 +19,55 @@ router = APIRouter(
 db = Depends(get_db)
 
 
-@router.post("", response_model=user_schema.Response.UserRead)
-def create_user(user: user_schema.Request.UserCreate, db: Session = db):
+@router.post("", response_model=user_schema.Response.UserReadDetail)
+def create_user(
+    user: user_schema.Request.UserCreate,
+    db: Session = db
+):
     db_user = user_account_service.get_user_by_email(db=db, email=user.email)
-    
+
     if db_user:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="E-mail already exists."
-        )
+        raise user_already_exist_exception
 
     return user_account_service.create_user(db=db, user=user)
 
 
 @router.get("", response_model=user_schema.Response.UserRead)
-def read_user(email: str, db: Session = db):
+async def read_user(
+    email: str,
+    current_user: str = Depends(get_current_user),
+    db: Session = db
+):
     db_user = user_account_service.get_user_by_email(db=db, email=email)
-    
+
     if not db_user:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="E-mail does not exist."
-        )
+        raise user_not_exist_exception
 
     return db_user
 
 
-@router.put("", response_model=user_schema.Response.UserRead)
-def update_user(user: user_schema.Request.UserCreate, db: Session = db):
-    db_user = user_account_service.get_user_by_email(db=db, email=user.email)
-    
+@router.put("", response_model=user_schema.Response.UserReadDetail)
+async def update_user(
+    user: user_schema.Request.UserUpdate,
+    current_user: str = Depends(get_current_user),
+    db: Session = db
+):
+    db_user = user_account_service.get_user_by_email(db=db, email=current_user)
+
     if not db_user:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="E-mail does not exist."
-        )
+        raise user_not_exist_exception
 
-    return user_account_service.update_user(db=db, user=user)
+    return user_account_service.update_user(db=db, email=current_user, user=user)
 
 
-@router.delete("", response_model=user_schema.Response.UserRead)
-def delete_user(email: str, db: Session = db):
-    db_user = user_account_service.get_user_by_email(db=db, email=email)
-    
+@router.delete("", response_model=user_schema.Response.UserReadDetail)
+async def delete_user(current_user: str = Depends(get_current_user), db: Session = db):
+    db_user = user_account_service.get_user_by_email(db=db, email=current_user)
+
     if not db_user:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="E-mail does not exist."
-        )
+        raise user_not_exist_exception
 
-    return user_account_service.delete_user(db=db, email=email)
+    return user_account_service.delete_user(db=db, email=current_user)
 
 
 @router.get("/all", response_model=list[user_schema.Response.UserRead])
