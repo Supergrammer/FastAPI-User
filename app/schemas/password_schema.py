14b812e3b3baaa47e37fe3f71bb524f88d8fbbaa
Supergrammer <supergrammer@naver.com>
2022-04-18 23:04:24 +0900

    [Feat] password_schema.py Request 클래스 추가, #6

--- app/schemas/password_schema.py
+++ app/schemas/password_schema.py
@@ -1,4 +1,4 @@
-from pydantic import UUID4
+from pydantic import BaseModel, UUID4, validator
 
 from .base_schema import BaseSchema
 
@@ -10,3 +10,25 @@ class Password(BaseSchema):
 
     class Config:
         orm_mode = True
+
+
+class Request():
+    class PasswordUpdate(BaseModel):
+        current_password: str
+        new_password: str
+        confirm_password: str
+
+        @validator("new_password")
+        def new_password_validate(cls, v, values):
+            if "current_password" in values and v == values["current_password"]:
+                raise ValueError("입력한 비밀번호와 바꾸려는 비밀번호가 동일합니다.")
+            return v
+
+        @validator("confirm_password")
+        def password_match(cls, v, values):
+            if "new_password" in values and v != values["new_password"]:
+                raise ValueError("새 비밀번호와 확인 비밀번호가 일치하지 않습니다.")
+            return v
+
+        class Config:
+            orm_mode = True
