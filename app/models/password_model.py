7e3c17fc7ce139ec3bffa712bfb51002fd224ad4
Supergrammer <supergrammer@naver.com>
2022-04-20 22:43:18 +0900

    [Feat] Password 모델에 password_history relationship 추가, #6

--- app/models/password_model.py
+++ app/models/password_model.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, String, Boolean
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 
 import uuid
 
@@ -12,4 +13,13 @@ class Password(Base, BaseMixin):
     id = Column(UUID(as_uuid=True), primary_key=True,
                 nullable=False, index=True, default=uuid.uuid4)
     hashed_password = Column(String, nullable=False)
+
+    password_history = relationship(
+        "PasswordHistory",
+        backref="password",
+        uselist=True,
+        primaryjoin="foreign(Password.id) == PasswordHistory.id",
+        order_by="desc(PasswordHistory.created_date)"
+    )
+
     is_expired = Column(Boolean, nullable=False, default=False)
