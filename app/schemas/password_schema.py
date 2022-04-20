c725f9744ace4ef51e2041de5e7f691d41bcf275
Supergrammer <supergrammer@naver.com>
2022-04-20 22:45:49 +0900

    [Feat] password_schema validator 업데이트, #6

--- app/schemas/password_schema.py
+++ app/schemas/password_schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, UUID4, validator
+from pydantic import BaseModel, UUID4, Field, validator
 
 from .base_schema import BaseSchema
 
@@ -19,13 +19,17 @@ class Request():
         confirm_password: str
 
         @validator("new_password")
-        def new_password_validate(cls, v, values):
+        def new_password_validate(cls, v):
+            return v
+
+        @validator("new_password")
+        def password_match(cls, v, values):
             if "current_password" in values and v == values["current_password"]:
                 raise ValueError("입력한 비밀번호와 바꾸려는 비밀번호가 동일합니다.")
             return v
 
         @validator("confirm_password")
-        def password_match(cls, v, values):
+        def new_password_match(cls, v, values):
             if "new_password" in values and v != values["new_password"]:
                 raise ValueError("새 비밀번호와 확인 비밀번호가 일치하지 않습니다.")
             return v
