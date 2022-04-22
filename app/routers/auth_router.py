5d1df4d36481cc5496160fac111843f38a805e39
Supergrammer <supergrammer@naver.com>
2022-04-22 22:32:37 +0900

    [Fix] auth_router 수정, #5

--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -3,13 +3,11 @@ from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from app.configurations.database import get_db
-from app.services import user_auth_service
-
-from app.schemas import user_schema
-
 from app.modules import auth_module
 
-from app.http_exception import invalid_user_exception, credentials_exception
+from app.services import auth_service
+
+from app.http_exception import invalid_user_exception
 
 router = APIRouter(
     prefix="/auth",
@@ -24,7 +22,7 @@ async def login(
     form_data: OAuth2PasswordRequestForm = Depends(),
     db: Session = db
 ):
-    db_user = user_auth_service.authenticate_user(
+    db_user = auth_service.authenticate_user(
         db=db, email=form_data.username, password=form_data.password)
 
     if not db_user:
@@ -51,7 +49,7 @@ async def login_check(
     current_user: str = Depends(auth_module.get_current_user),
     db: Session = db
 ):
-    db_user = user_auth_service.authenticate_user(
+    db_user = auth_service.authenticate_user(
         db=db, email=current_user, password=password)
 
     if not db_user:
