fbdc678241b8409eebe5ede6e38a14ad6ecee034
Supergrammer <supergrammer@naver.com>
2022-04-22 21:15:57 +0900

    [Feat] user_account >> user 변경 및 코드 정리, #4

--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -1,14 +1,13 @@
-import email
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from app.configurations.database import get_db
 from app.modules.auth_module import get_current_user
 
-from .user_password_router import router as user_password_router
-
 from app.schemas import user_schema
 from app.services import user_account_service
+from app.services.common import get_user_by_email
+
 from app.http_exception import user_already_exist_exception, user_not_exist_exception, credentials_exception
 
 
@@ -16,8 +15,6 @@ router = APIRouter(
     prefix="/users",
     tags=["user"],
 )
-
-router.include_router(user_password_router)
 
 db = Depends(get_db)
 
@@ -27,7 +24,7 @@ def create_user(
     user: user_schema.Request.UserCreate,
     db: Session = db
 ):
-    db_user = user_account_service.get_user_by_email(db=db, email=user.email)
+    db_user = get_user_by_email(db=db, email=user.email)
 
     if db_user:
         raise user_already_exist_exception
@@ -41,7 +38,7 @@ async def read_user(
     current_user: str = Depends(get_current_user),
     db: Session = db
 ):
-    db_user = user_account_service.get_user_by_email(db=db, email=email)
+    db_user = get_user_by_email(db=db, email=email)
 
     if not db_user:
         raise user_not_exist_exception
@@ -55,46 +52,48 @@ async def update_user(
     current_user: str = Depends(get_current_user),
     db: Session = db
 ):
-    db_user = user_account_service.get_user_by_email(
-        db=db, current_user=current_user)
+    db_current_user = get_user_by_email(db=db, email=current_user)
 
-    if not db_user:
+    if not db_current_user:
         raise credentials_exception
 
-    return user_account_service.update_user(db=db, email=current_user, user=user)
+    return user_account_service.update_user(db=db, current_user=current_user, user=user)
 
 
 @router.delete("", response_model=user_schema.Response.UserReadDetail)
-async def delete_user(current_user: str = Depends(get_current_user), db: Session = db):
-    db_user = user_account_service.get_user_by_email(
-        db=db, current_user=current_user)
+async def delete_user(
+    current_user: str = Depends(get_current_user),
+    db: Session = db
+):
+    db_current_user = get_user_by_email(db=db, email=current_user)
 
-    if not db_user:
+    if not db_current_user:
         raise credentials_exception
 
-    return user_account_service.delete_user(db=db, email=current_user)
+    return user_account_service.delete_user(db=db, current_user=current_user)
 
 
 @router.get("/me", response_model=user_schema.Response.UserReadDetail)
-async def read_user_me(current_user: str = Depends(get_current_user), db: Session = db):
-    db_user = user_account_service.get_user_by_email(db=db, email=current_user)
+async def read_user_me(
+    current_user: str = Depends(get_current_user),
+    db: Session = db
+):
+    db_current_user = get_user_by_email(db=db, email=current_user)
 
-    if not db_user:
+    if not db_current_user:
         raise credentials_exception
 
-    return db_user
+    return db_current_user
 
 
 @router.get("/me/active", response_model=user_schema.Response.UserReadDetail)
-async def read_user_me_active(current_user: str = Depends(get_current_user), db: Session = db):
-    db_user = user_account_service.get_user_by_email(db=db, email=current_user)
+async def read_user_me_active(
+    current_user: str = Depends(get_current_user),
+    db: Session = db
+):
+    db_current_user = get_user_by_email(db=db, email=current_user)
 
-    if not db_user or not db_user.is_active:
+    if not db_current_user or not db_current_user.is_active:
         raise credentials_exception
 
-    return db_user
-
-
-@router.get("/all", response_model=list[user_schema.Response.UserRead])
-def read_all_users(skip: int | None = None, limit: int | None = None, db: Session = db):
-    return user_account_service.get_all_users(db=db, skip=skip, limit=limit)
+    return db_current_user
