a67f38be9effee494581eb84395968b1a829d172
Supergrammer <supergrammer@naver.com>
2022-04-30 12:02:40 +0900

    [Feat] Add .gitignore #3

--- setup.sh
+++
@@ -1,8 +0,0 @@
-# Add GitHub Label
-
-LABELS_DIR=".GitHub_Label"
-
-ACCESS_TOKEN=$(<$LABELS_DIR/.accessToken)
-REPOSITORY_NAME="Toy-Project-Template"
-
-github-label-sync --access-token $ACCESS_TOKEN --labels $LABELS_DIR/labels.json Supergrammer/$REPOSITORY_NAME
