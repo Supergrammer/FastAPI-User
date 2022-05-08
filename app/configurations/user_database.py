e10c83190a548a6cdb2db7180a5984d0431d2c11
Supergrammer <supergrammer@naver.com>
2022-05-08 18:53:05 +0900

    [Feat] database.py >> user_database.py, #3

--- app/configurations/user_database.py
+++ app/configurations/user_database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-from .settings import get_database_settings
+from .settings import get_user_database_settings
 
-db = get_database_settings()
+db = get_user_database_settings()
 
 DATABASE_URL = f"{db.database}://{db.database_user}:{db.database_password}@{db.database_server}/{db.database_name}"
 
@@ -14,7 +14,7 @@ db_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
-def get_db():
+def get_user_db():
     db = db_session()
     try:
         yield db
