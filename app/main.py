db7ef949b00d97d790c6d2fa2b0861be301b1d2d
Supergrammer <supergrammer@naver.com>
2022-05-10 20:31:38 +0900

    [Feat] main.py 수정, #8

--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 
 from app.configurations.user_database import engine
-from app.configurations.redis import init_redis_pool
 
 from app.models import models
 from app.routers import routers
 
+
 models.Base.metadata.create_all(bind=engine)
-init_redis_pool("localhost", "rlaguscjf123")
 
 app = FastAPI()
 app.include_router(router=routers.router)
