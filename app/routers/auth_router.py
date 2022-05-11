682d6234b636252af1fdf5564496933b52da7301
Supergrammer <supergrammer@naver.com>
2022-05-11 18:00:34 +0900

    [Feat] /token/refresh, /logout 추가, #8

--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -1,14 +1,17 @@
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+from aioredis import Redis
 
 from app.configurations.user_database import get_user_db
+from app.configurations.redis import get_redis_pool
 from app.modules import auth_module
 
 from app.schemas import token_schema
 from app.services import auth_service
+from app.services.common import get_user_by_email
 
-from app.http_exception import invalid_user_exception
+from app.http_exception import invalid_user_exception, access_token_invalid_exception
 
 router = APIRouter(
     prefix="/auth",
@@ -16,13 +19,15 @@ router = APIRouter(
 )
 
 db = Depends(get_user_db)
+redis = Depends(get_redis_pool)
 
 
 @router.post("/login", response_model=token_schema.Response.TokenDetail)
 @router.post("/token", response_model=token_schema.Response.TokenDetail)
-def get_token(
+async def get_token(
     form_data: OAuth2PasswordRequestForm = Depends(),
-    db: Session = db
+    db: Session = db,
+    redis: Redis = redis
 ):
     db_user = auth_service.authenticate_user(
         db=db, email=form_data.username, password=form_data.password)
@@ -30,15 +35,22 @@ def get_token(
     if not db_user:
         raise invalid_user_exception
 
-    return auth_module.get_token(db_user.email)
+    return await auth_service.get_authentication(redis=redis, user=db_user)
 
 
 @router.post("/token/refresh", response_model=token_schema.Response.TokenDetail)
 async def refresh_token(
     token: token_schema.Request.TokenRefresh,
-    expired_user: str = Depends(auth_module.get_expired_user),
+    expired_user: str = Depends(auth_module.get_all_user),
+    db: Session = db,
+    redis: Redis = redis
 ):
-    return auth_module.refresh_expired_token(refresh_token=token.refresh_token, expired_user=expired_user)
+    db_user = get_user_by_email(db=db, email=expired_user)
+
+    if not db_user:
+        raise access_token_invalid_exception
+
+    return await auth_service.refresh_authentication(redis=redis, user=db_user, refresh_token=token.refresh_token)
 
 
 @router.post("/verify")
@@ -54,3 +66,20 @@ async def login_check(
         raise invalid_user_exception
 
     return {}
+
+
+@router.get("/logout")
+async def logout(
+    user: str = Depends(auth_module.get_all_user),
+    db: Session = db,
+    redis: Redis = redis
+):
+    db_user = get_user_by_email(db=db, email=user)
+
+    if not db_user:
+        raise access_token_invalid_exception
+
+    await auth_service.expire_authentication(redis=redis, user=db_user)
+
+    return {}
+    
